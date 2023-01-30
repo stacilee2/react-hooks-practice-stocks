a8b935577e0755af08963abd5419f55c3c4b4c32
Staci Rosales <stacileep2@gmail.com>
2023-01-30 09:27:46 -0600

    commit tree 9d2abe3f0f3641a7ad6db02600d5b70c70742e6c

--- src/components/StockContainer.js
+++ src/components/StockContainer.js
@@ -1,13 +1,27 @@
-import React from "react";
-import Stock from "./Stock";
+import React, { useState, useEffect } from "react";
+// import Stock from "./Stock";
 
 function StockContainer() {
+
+  const [stockList, setStockList] = useState([]);
+  
+  useEffect(() => {
+    fetch("http://localhost:3001/stocks")
+      .then((response) => response.json())
+      .then((stocks) => {
+        setStockList(stocks);
+      });
+  }, []);
+  // use an empty dependencies array, so we only run the fetch request ONCE
+
+
   return (
     <div>
       <h2>Stocks</h2>
-      {/* render stock list here*/}
+      {stockList.map((stocks) => stocks.name)}
     </div>
   );
 }
+
 
 export default StockContainer;
